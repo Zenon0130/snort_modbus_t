5d22c5e4da9ab241b965cd4acf3e30885553aeff
zenon <mrz60606@gmail.com>
2024-07-02 17:29:58 +0800

    server modified

--- modbus_server.py
+++ modbus_server.py
@@ -1,9 +1,17 @@
 import socket
+from scapy.all import *
+from scapy.contrib.modbus import *
+
+def handle_modbus_request(data):
+    modbus_request = ModbusADURequest(data)
+    print("Received Modbus request:", modbus_request.show(dump=True))
+
+    modbus_response = ModbusADUResponse(transId=modbus_request.transId) / ModbusPDU01ReadCoilsResponse()
+    response_data = bytes(modbus_response)
+    return response_data
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 s.bind(('0.0.0.0', 502))
-
 s.listen(5)
 
 while True:
@@ -16,5 +24,7 @@ while True:
             break
 
         print('Received', repr(data))
+        response = handle_modbus_request(data)
+        client.send(response)
 
     client.close()
