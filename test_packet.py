046d8b19be57c86f434d4e08fcea60cd19d9ff99
zenon <mrz60606@gmail.com>
2024-07-02 15:48:28 +0800

    add testing server and packet, also note

---
+++ test_packet.py
@@ -0,0 +1,6 @@
+from scapy.all import *
+from scapy.contrib.modbus import *
+
+packet = IP(dst="10.103.152.8") / TCP(dport=502) / ModbusADURequest(transId=1) / ModbusPDU01ReadCoilsRequest(startAddr=0x0000, quantity=0x0001)
+
+send(packet)
